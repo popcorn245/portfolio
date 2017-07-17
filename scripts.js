009c31c713d9f63e8f8eceb77c5056eebfefcc0a
Bobby Johnson <bobby@madnesslabs.net>
2017-07-17 13:08:23 -0500

    Add jquery functionality to form submission

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    $('#contactForm').on('submit', submitContactForm);
+
+});
+
+
+
+function submitContactForm() {
+    event.preventDefault();
+    var x = document.forms["contactForm"]["email"].value;
+    if (x == "") {
+        alert("Email must be filled out");
+        return false;
+    } else {
+        $(this).slideUp();
+        $("#success").slideDown();
+        //alert('Thank you for submitting the form');
+    }
+}
